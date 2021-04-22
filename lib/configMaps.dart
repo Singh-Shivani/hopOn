c9670184a3c3f473e1fc3ea94a86ee5292a79ca1
Shivani Singh <shivanisingh260500@gmail.com>
2021-04-22 18:42:52 +0530

    new api key for geocoding through google maps platform added

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -1,2 +1,2 @@
 String apiKey = 'AIzaSyBB34ETGgqbdUe4LRRcNF7lmY8XWdFE7NM';
-String geocodingApi = 'AIzaSyC4vjGTiCTlYYe1eBlRJGmmTjlBLiw2k5A';
+String geocodingApi = 'AIzaSyBr68q9oOcrRNPwgqumzv3uFCrKJdIt_iY';

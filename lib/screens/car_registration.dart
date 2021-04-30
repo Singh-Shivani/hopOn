b4cfaaa7e3d91c549b8d011fe8beeefb8d4d696d
Shivani Singh <shivanisingh260500@gmail.com>
2021-04-30 18:09:18 +0530

    renamed car_registration.dart

--- lib/screens/car_registration.dart
+++ lib/screens/car_registration.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vehicle_sharing_app/models/user.dart';
 import 'package:vehicle_sharing_app/screens/owner_homePage.dart';
 import 'package:vehicle_sharing_app/services/firebase_services.dart';
 import 'package:vehicle_sharing_app/services/validation_services.dart';
 import 'package:vehicle_sharing_app/widgets/widgets.dart';
-import 'package:connectivity/connectivity.dart';
 
 class VehicleDetails extends StatefulWidget {
   @override
@@ -16,10 +14,11 @@ class _VehicleDetailsState extends State<VehicleDetails> {
   final _formKey = GlobalKey<FormState>();
 
   TextEditingController _modelNameController = TextEditingController();
-  TextEditingController _vehicleNumberController= TextEditingController();
+  TextEditingController _vehicleNumberController = TextEditingController();
   TextEditingController _ownerNameController = TextEditingController();
   TextEditingController _colorController = TextEditingController();
   TextEditingController _aadharcardController = TextEditingController();
+  TextEditingController _rentAmount = TextEditingController();
 
   VehicleUser owner = VehicleUser();
 
@@ -28,10 +27,11 @@ class _VehicleDetailsState extends State<VehicleDetails> {
   void initVehicleUser() {
     owner.modelName = _modelNameController.text;
     owner.vehicleNumber = _vehicleNumberController.text;
-    owner.ownerName= _ownerNameController.text;
+    owner.ownerName = _ownerNameController.text;
     owner.color = _colorController.text;
     owner.aadharNumber = _aadharcardController.text;
     owner.hasCompletedRegistration = true;
+    owner.amount = _rentAmount.text;
   }
 
   @override
@@ -95,11 +95,19 @@ class _VehicleDetailsState extends State<VehicleDetails> {
                       controller: _aadharcardController,
                     ),
                     SizedBox(
+                      height: 0.03 * deviceSize.height,
+                    ),
+                    InputFormField(
+                      fieldName: 'Rent amount',
+                      obscure: false,
+                      // validator: ValidationService().aadharNumberValidator,
+                      controller: _aadharcardController,
+                    ),
+                    SizedBox(
                       height: 0.05 * deviceSize.height,
                     ),
                     GestureDetector(
                       onTap: () async {
-
                         ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(content: Text('Processing')));
                         initVehicleUser();
@@ -128,7 +136,9 @@ class _VehicleDetailsState extends State<VehicleDetails> {
                         }
                       },
                       child: CustomButton(
-                        text: 'Submit',color:Colors.black, textColor: Colors.white,
+                        text: 'Submit',
+                        color: Colors.black,
+                        textColor: Colors.white,
                       ),
                     ),
                   ],

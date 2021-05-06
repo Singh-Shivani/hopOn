1bdade1c32ea5653a3d93cef03d5ec2623681b4c
Shivani Singh <shivanisingh260500@gmail.com>
2021-05-07 01:41:29 +0530

    Removed API Key

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -1,2 +1,3 @@
-String apiKey = 'AIzaSyBB34ETGgqbdUe4LRRcNF7lmY8XWdFE7NM';
-String geocodingApi = 'AIzaSyBr68q9oOcrRNPwgqumzv3uFCrKJdIt_iY';
+String apiKey = 'YOUR_ANDROID/iOS_APK_API_KEY';
+String geocodingApi = 'YOUR_GEOCODING_API_KEY';
+//(This geocoding api also works for places api and directions api if you have restricted the api key with these 3 services)
